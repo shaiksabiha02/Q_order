b2eda79843e4d7a1103415c615fd85c117094045
Gowri-Paithacs <vasimalla@paithacs.com>
2026-09-16 11:58:43 +0530

    updated webhook file

--- src/tests/unit/integration/test.webhook.js
+++ src/tests/unit/integration/test.webhook.js
@@ -11,7 +11,7 @@ const payload = JSON.stringify({
     payment: {
       entity: {
         id: "pay_test123",
-        order_id: "order_Tafo6pbAquFOh2"
+        order_id: "order_TcBF4GSvXKyPrK"
       }
     }
   }
